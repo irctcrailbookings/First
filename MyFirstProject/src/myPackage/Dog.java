d91b1ea442757cffd0a40d831624fa75bf1a4cb4
irctcrailbookings <smsubhashp@hotmail.com>
2020-04-22 21:42:04 +0530

    Revert "User#Subhash Dog class updated"
    This reverts commit b40dd2123c651f9e50ff427c44e062427a018985.

--- MyFirstProject/src/myPackage/Dog.java
+++ MyFirstProject/src/myPackage/Dog.java
@@ -5,6 +5,11 @@ public class Dog {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+<<<<<<< HEAD
+		//This is my second commit
+=======
+		
+>>>>>>> parent of 897dd1a... User#Subhash This is my seconf commit
 
 	}
 
