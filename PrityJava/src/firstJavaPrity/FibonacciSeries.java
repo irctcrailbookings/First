33cb42e812205050691784083e74ed50dd1135f1
Guddua1001 <guddua1001@gmail.com>
2020-04-22 23:58:20 +0530

    User#Prity My Project Coommit.

---
+++ PrityJava/src/firstJavaPrity/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package firstJavaPrity;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+	int n1=0,n3;
+	int n2=1;
+	System.out.print(n1+" "+n2);
+	for(int i=0;i<10;i++)
+	{
+		n3=n1+n2;
+		n1=n2;
+		n2=n3;
+		System.out.print(" "+n3+" ");
+	}
+	}
+
+}
