84b8655ee6d44e4a0eda6a30b9c5a9f2935bcad7
Guddua1001 <guddua1001@gmail.com>
2020-04-22 22:29:21 +0530

    User#Prity commiting through her username and password

---
+++ MyFirstProject/src/mySecondPackage/PrityFactorialSecond.java
@@ -0,0 +1,21 @@
+package mySecondPackage;
+
+import java.util.Scanner;
+
+public class PrityFactorialSecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Please enter a number of your choice");
+		int n=sc.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact= fact*i;
+		}
+		System.out.println("Factorial of "+n+ " 5is= " +fact);
+		
+	}
+
+}
