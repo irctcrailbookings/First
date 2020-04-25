8e6932d37e4070f34fb55e0be20a1446805e4b48
irctcrailbookings <smsubhashp@hotmail.com>
2020-04-25 22:43:10 +0530

    deleted un wanted file.

--- MyFirstProject/src/myPackage/Lion.java
+++ MyFirstProject/src/myPackage/Lion.java
@@ -1,22 +1,20 @@
 package myPackage;
 
 public class Lion {
-	private String color= "Yellow";
-	private int weight= 45;
-	
-	public Lion()
-	{
+	private String color = "Yellow";
+	private int weight = 45;
+
+	public Lion() {
 		System.out.println(color);
 		System.out.println(weight);
 	}
-	
-	public void speak(String s)
-	{
+
+	public void speak(String s) {
 		System.out.println(s);
 	}
-	
-	public void run (String r)
-	
+
+	public void run(String r)
+
 	{
 		System.out.println(r);
 	}
