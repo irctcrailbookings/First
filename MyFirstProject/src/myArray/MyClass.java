17098b2151e9233b8a7608b717c59dd8d9fb84e6
pragya1702 <pragyaprasad2011@gmail.com>
2020-04-26 19:10:00 +0530

    User=Pragya String array Merge and Commit

--- MyFirstProject/src/myArray/MyClass.java
+++ MyFirstProject/src/myArray/MyClass.java
@@ -1,13 +1,19 @@
 package myArray;
 
 public class MyClass {
+
+	static String[] si = { "Subhash", "Pragya", "Prity", "Vaibhav" };
 	static String[] s = { "Subhash", "Pragya", "Prity", "Vaibhav" };
 
-	public static void main(String[] args){
-	for(int i=0; i<s.length;i++){
-	System.out.println(s[i]);
-	//System.out.println(Arrays.toString[s]);
-	}
+	public static void main(String[] args) {
+		for (int k = 0; k < si.length; k++) {
+			System.out.println(si[k]);
+		}
+
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+			// System.out.println(Arrays.toString[s]);
+		}
 	}
 
 }
