3dd048ad23c1c30da21a1d390bb21eb69dd79c25
irctcrailbookings <smsubhashp@hotmail.com>
2020-04-26 20:39:06 +0900

    loop#vaibhav table 3

--- MyFirstProject/src/myLoop/MyClass.java
+++ MyFirstProject/src/myLoop/MyClass.java
@@ -3,7 +3,11 @@ package myLoop;
 public class MyClass {
 	
 	public static void main(String[] args) {
-		
+		for (int i=1; i<=10; i++)
+		{
+			System.out.println("3"+"X"+i+"="+3*i);
+			
+		}	
 
 	}
 
