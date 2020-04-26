248f97d684a819fcfd9bd56abcb2940caed6b712
irctcrailbookings <smsubhashp@hotmail.com>
2020-04-26 18:10:52 +0530

    Loop commit

--- MyFirstProject/src/myLoop/MyClass.java
+++ MyFirstProject/src/myLoop/MyClass.java
@@ -1,22 +1,21 @@
 package myLoop;
 
 public class MyClass {
-	
+
 	public static void main(String[] args) {
-		for (int i=1; i<=10; i++)
-		{
-			System.out.println("3"+"X"+i+"="+3*i);
-			
-		}	
-		for(int i = 1; i <= 10; i++) {
-			System.out.println("2"+"X"+i+"="+2*i);
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("3" + "X" + i + "=" + 3 * i);
+
+		}
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("2" + "X" + i + "=" + 2 * i);
 		}
 
 		for (int i = 1; i <= 10; i++) {
-			System.out.println("4"+"x"+i+"="+ 4*i);
+			System.out.println("4" + "x" + i + "=" + 4 * i);
 		}
 		for (int i = 1; i <= 10; i++) {
-			System.out.println("5"+"x"+i+"="+ 5*i);
+			System.out.println("5" + "x" + i + "=" + 5 * i);
 		}
 	}
 
