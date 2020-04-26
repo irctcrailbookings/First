50b076e98c2f164d4f4c07ea0bc0a8d2cb6579dd
Guddua1001 <guddua1001@gmail.com>
2020-04-26 18:45:43 +0530

    user#Prity committing Array

--- MyFirstProject/src/myArray/MyClass.java
+++ MyFirstProject/src/myArray/MyClass.java
@@ -1,6 +1,13 @@
 package myArray;
 
 public class MyClass {
-	
+	static String[] s = { "Subhash", "Pragya", "Prity", "Vaibhav" };
+
+	public static void main(String[] args){
+	for(int i=0; i<s.length;i++){
+	System.out.println(s[i]);
+	//System.out.println(Arrays.toString[s]);
+	}
+	}
 
 }
