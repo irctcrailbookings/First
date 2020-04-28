cce0ef48388f38cc9868fdd5ee62d4db1044c099
Vairidhi <vaibhavb2792@gmail.com>
2020-04-28 22:54:45 +0900

    User#Vaibhav While loop Practice

---
+++ MyFirstProject/src/myLoop/myWhileloop.java
@@ -0,0 +1,18 @@
+package myLoop;
+
+public class myWhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1;
+		while (i<366) 
+		{
+		System.out.println ("DAY"+i);
+		i++;
+		
+		}
+
+	}
+
+}
