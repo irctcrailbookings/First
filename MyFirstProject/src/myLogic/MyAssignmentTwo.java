b261abf6037f6bb9abbde18eb96f5d5d8ee39130
Guddua1001 <guddua1001@gmail.com>
2020-04-29 21:35:08 +0530

    User#Prity committing code for Triangle and leapYear

--- MyFirstProject/src/myLogic/MyAssignmentTwo.java
+++ MyFirstProject/src/myLogic/MyAssignmentTwo.java
@@ -12,7 +12,12 @@ public class MyAssignmentTwo {
 	}
 	
 	public static void leapYearP(int number) {
-		
+		if(number%4==0 && number%100==0 && number%400==0) {
+			System.out.println(number+" is a leap year");
+		}
+		else {
+			System.out.println(number+" is not a leap year");
+		}
 	}
 	
 	public static void determineTrainagleV(int a, int b , int c) {
@@ -20,7 +25,13 @@ public class MyAssignmentTwo {
 	}
 	
 	public static void determineTrainagleP(int a, int b , int c) {
-
+		if(a+b >c && b+c>a && c+a>b)
+		{
+			System.out.println("Yes it is a triangle");
+		}
+		else {
+			System.out.println("No it is not a triangle");
+		}
 	}
 	
 	public static void printLeapYearP(int yearOne, int yearTwo ) {
