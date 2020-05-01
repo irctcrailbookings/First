f7c90dbdc9e0ecf495a07014382b1ae689238df1
Vairidhi <vaibhavb2792@gmail.com>
2020-05-01 14:33:31 +0900

    User#Vaibhav Assignment3

--- MyFirstProject/src/myLogic/MyAssignmentThree.java
+++ MyFirstProject/src/myLogic/MyAssignmentThree.java
@@ -1,33 +1,83 @@
 package myLogic;
 
+import sun.security.util.Length;
+
 public class MyAssignmentThree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		pattern();
+
 		String[] name = { "ok", "pk", "dk", "ck", "ok", "pk", "ok", "lk", "ok" };
+
 		countWordOK(name);
-		countWord(name, "lk"); 
+		countWord(name, "pk");
 	}
 
 	public static void countWordOK(String[] arr) {
+
+		int count = 0;
+
+		for (int l = 0; l < arr.length; l++) {
+			if (arr[l] == "ok") {
+				count++;
+							}
+
+		}
+
+		System.out.println("Total count of Ok is " + count);
 
 	}
 
 	public static void countWord(String[] arr, String word) {
 
+		int i = 0;
+		for (int s = 0; s < arr.length; s++) {
+			if (arr[s] == word)
+
+			{
+				i++;
+			}
+
+		}
+
+		System.out.println("Total no. repetetion of " + word + " is " + i);
+
 	}
-	
+
 	public static void MaxReoccurence(String[] arr) {
-		
-	} 
-	
-	/* +++
-	 * ++
-	 * +
-	 * */
-	
+
+	}
+
+	/*
+	 * +++ ++ +
+	 */
+
 	public static void pattern() {
-		int i = 5;
+//		int i = 5;
+//
+//		for (int k = 4; k > 0; k--) {
+//			if (k == 3) {
+//				System.out.println("+++");
+//
+//			}
+//			if (k == 2) {
+//				System.out.println("++");
+//			}
+//			if (k==1) {
+//				System.out.println("+");
+//			}
+//		
+//		
+//		}
+		for (int j = 5; j > 0; j--) {
+
+			for (int p = j; p > 0; p--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
 	}
 
 }
